0b4a38c1705ba5657f43c7b063843ca421240df0
Rahul Barua <rahul.ctg62@gmail.com>
2022-09-22 09:39:39 +0600

    initial commit
    initial commit

---
+++ src/test/java/com/rahul/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
